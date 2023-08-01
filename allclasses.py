4f407ab485dab9a84bec883053988c285fb8a54e
Mieledickt <132281244+Mieledickt@users.noreply.github.com>
2023-08-01 19:55:38 +0300

    newbot

---
+++ allclasses.py
@@ -0,0 +1,55 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        self.name = value
+        super().__init__(value=value)
+
+
+class Phone(Field):
+    def __init__(self, value=''):
+        self.phone = value
+        super().__init__(value=value)
+
+
+class Record:
+    def __init__(self, name, phones=None):
+        self.name = name
+        self.phones = phones if phones is not None else []
+
+    def add_phone(self, phone):
+        if not phone.value in [p.value for p in self.phones]:
+            self.phones.append(phone)
+
+    def delete_phone(self, phone):
+        self.phones = [p for p in self.phones if str(p) != str(phone)]
+
+    def change_phone(self, old_phone, new_phone):
+        index = [str(phone) for phone in self.phones].index(str(old_phone))
+        self.phones[index] = new_phone
+
+    def __str__(self):
+        return f"{self.phones}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def update_record(self, record):
+        self.data[record.name.value].phones.extend(record.phones)
